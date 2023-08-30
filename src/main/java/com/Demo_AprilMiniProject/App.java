d8414e3d12041b946d6fe26c053dba99bfcf68d0
rashmiuppare <rashmiuppare@yahoo.co.in>
2023-08-30 13:30:55 +0530

    project added

---
+++ src/main/java/com/Demo_AprilMiniProject/App.java
@@ -0,0 +1,13 @@
+package com.Demo_AprilMiniProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
